4f22f0a006eca1b40380595d163673fab907ed49
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-08-10 22:55:34 +0545

    Day:8, implement Delete from DB,single page dynamic blog,ejs includes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const { blogs } = require("./model/index");
+const path = require('path');
 
 require("./model/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.set("viewengine", "ejs");
 
@@ -18,6 +20,28 @@ app.get("/", async (req, res) => {
 app.get("/addBlog", (req, res) => {
   res.render("addBlog.ejs");
 });
+
+app.get('/single/:id',async(req,res)=>{
+  const {id } = req.params;
+
+  const blog = await blogs.findByPk(id);
+  const {title,subtitle,description} = blog;
+  // console.log(title,subtitle,description)
+  // another way of retriving data
+  // const blog = awiat blogs.findAll({
+  //   where:{
+  //     id,
+  //   },
+  // })
+
+  res.render("singleBlog.ejs",{title,subtitle,description,id});
+})
+
+app.get('/delete/:id',async(req,res)=>{
+  const id = req.params.id;
+  await blogs.destroy({ where: { id } });
+  res.redirect('/');
+})
 
 app.post("/createBlog", async (req, res) => {
   const { title, subtitle, description } = req.body;
